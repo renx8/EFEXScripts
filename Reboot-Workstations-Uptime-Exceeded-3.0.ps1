fa8366a503c34f1ac41bbb2ad8419b4ebc35759d
renx8 <108107647+renx8@users.noreply.github.com>
2023-07-04 22:07:51 +1000

    Reboot-Workstations-Uptime-Exceeded-3.1.ps1

--- Reboot-Workstations-Uptime-Exceeded-3.0.ps1
+++ Reboot-Workstations-Uptime-Exceeded-3.0.ps1
@@ -1,6 +1,23 @@
 # reboot Script V3.0 
 # This script will reboot any machines that have a pending reboot.
 
+#remain hidden
+# .Net methods for hiding/showing the console in the background
+Add-Type -Name Window -Namespace Console -MemberDefinition '
+[DllImport("Kernel32.dll")]
+public static extern IntPtr GetConsoleWindow();
+
+[DllImport("user32.dll")]
+public static extern bool ShowWindow(IntPtr hWnd, Int32 nCmdShow);
+'
+function Hide-Console
+{
+    $consolePtr = [Console.Window]::GetConsoleWindow()
+    #0 hide
+    [Console.Window]::ShowWindow($consolePtr, 0)
+}
+Hide-Console
+
 # Hash table to store registry checks
 <#PSScriptInfo
  
